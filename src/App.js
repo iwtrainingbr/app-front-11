a3cca574fb87d49752a103034006b62af979bcd4
Alessandro Feitoza <alessandrofeitoza01@gmail.com>
2022-01-07 21:43:29 -0300

    Add page de carrinho

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import List from "./pages/List";
 import NotFound from "./pages/NotFound";
 import Config from "./pages/Config";
 import Example from "./pages/Example";
+import Carrinho from "./pages/Carrinho";
 
 export default function App() {
   return (
@@ -15,11 +16,12 @@ export default function App() {
         <Link to="/config">Configuração </Link>
         <Link to="/listar">Listar</Link>
         <Link to="/exemplo">Exemplo</Link>
+        <Link to="/carrinho">Carrinho</Link>
       </div>
-
 
       <Routes>
         <Route path="/" element={<h1>Inicio</h1>}/>
+        <Route path="/carrinho" element={<Carrinho/>} />
         <Route path="/login" element={<Login/>} />
         <Route path="/listar" element={<List/>} />
         <Route path="/config" element={<Config/> }/>
