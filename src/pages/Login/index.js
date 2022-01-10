4dc9f7e521d45b2bf3b5f74aa4a492795dbb6d42
Chagas Barros <francisco_publicidade@yahoo.com.br>
2022-01-10 10:45:22 -0300

    adicionei um esboço da navbar

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,24 +1,50 @@
-
-function Input ({type, label}) {
-  return (
-    <>
-      <label>{label}</label>
-      <input className="form-control mb-2" type={type} placeholder={label}/>
-    </>
-  )
-}
-
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import './style.css';
 
 
 export default function Login() {
   return (
-    <div>
-        <Input type="email" label="Email"/>
-        <Input type="password" label="Senha"/>
+    <Box
+        component="form"
+       sx={{
+         width: 400,
+         height: 500,
+         backgroundColor:'#454d6b',
+         color: '#ffffff',
+         ml: 60,
+         mt: 10
+       }}
 
-        <Input type="text" label="Nome"/>
+    >
+    <form>
+          <div>
+              <h1 className="head">Login</h1>
+              <Divider/>
+          </div>
+          <div className="acesso">
+            <TextField color="secondary" fullWidth label="Usuário" variant="standard" /><br/>
+            <TextField color="secondary" fullWidth label="Senha" variant="standard"/> <br/>
 
-      <button className="btn btn-primary">Entrar</button>
-    </div>
-  )
+          </div>
+
+          <div className="button">
+              <Button fullWidth color="secondary" size="large" variant="contained">Login</Button>
+          </div>
+          <div className="ancora" >
+
+              <Link href="#" color="white" underline="none">
+                  {'Esqueceu a senha?'}
+              </Link>
+          </div>
+
+
+      </form>
+
+    </Box>
+  );
 }
