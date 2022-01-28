18f1b82d3f80cfc71d4c1b06ee9e6152684f0b35
marikinha <mari.cavalcante.0823@gmail.com>
2022-01-28 18:50:59 -0300

    implementei API na tela de perfis

--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -1,7 +1,6 @@
 
 import {useState} from 'react';
 import {useNavigate} from "react-router-dom";
-
 import {Box} from '@mui/material';
 import Button from '@mui/material/Button';
 import Divider from '@mui/material/Divider';
